7ea2a53b910dfcc7b3a9d448c5a9c13177c09dab
itang1 <irenetang14@gmail.com>
2025-07-24 23:51:27 -0700

    Initial Commit

---
+++ freebnb/freebnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  freebnbApp.swift
+//  freebnb
+//
+//  Created by Irene Tang on 7/24/25.
+//
+
+import SwiftUI
+
+@main
+struct freebnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ freebnbTests/freebnbTests.swift
@@ -0,0 +1,16 @@
+//
+//  freebnbTests.swift
+//  freebnbTests
+//
+//  Created by Irene Tang on 7/24/25.
+//
+
+import Testing
+
+struct freebnbTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
