e53821ca60a761e08cf218998a8a27f9e8818235
itang1 <irenetang14@gmail.com>
2025-08-09 00:57:58 -0700

    Added color palette

---
+++ freebnb/Extensions.swift
@@ -0,0 +1,23 @@
+//
+//  Extensions.swift
+//  freebnb
+//
+//  Created by Irene Tang on 8/9/25.
+//
+
+import SwiftUI
+
+struct FlippedPrimaryColor: ViewModifier {
+    @Environment(\.colorScheme) var colorScheme
+
+    func body(content: Content) -> some View {
+        content
+            .foregroundColor(colorScheme == .dark ? .black : .white)
+    }
+}
+
+extension View {
+    func flippedPrimaryColor() -> some View {
+        self.modifier(FlippedPrimaryColor())
+    }
+}
